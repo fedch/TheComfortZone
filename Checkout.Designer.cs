8a0e0ad922b304f89c90be272ef17be73d19781a
Diana Gonzalez <vanessitagon@hotmail.com>
2021-05-12 11:35:32 +1200

    merge

--- Checkout.Designer.cs
+++ Checkout.Designer.cs
@@ -16,7 +16,7 @@ namespace TheComfortZone
         {
             if (disposing && (components != null))
             {
-                components.Dispose();
+                components.Dispose(); 
             }
             base.Dispose(disposing);
         }
